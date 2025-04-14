f500d6a228e40ae8bf994c26f38d0d0d166179b9
yujianfx <yujianfx@shouhui-tech.com>
2025-04-14 19:56:50 +0800

    init

--- src/BaseClient.php
+++ src/BaseClient.php
@@ -21,15 +21,15 @@ class BaseClient {
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => $callUrl,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 60,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => '',
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 60,
+//            CURLOPT_FOLLOWLOCATION => true,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS => $params,
-            CURLOPT_SSL_VERIFYPEER => false,
+//            CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_HTTPHEADER => $headers,
         ));
 
