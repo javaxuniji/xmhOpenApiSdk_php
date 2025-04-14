a9e65b3f8f2521d217e1b918f1c823d0bfe5dcfd
yujianfx <yujianfx@shouhui-tech.com>
2025-04-14 18:59:27 +0800

    init

--- src/Common.php
+++ src/Common.php
@@ -2,15 +2,15 @@
 
 namespace XMH\OpenApiSdk;
 class XmhCommon {
-    const BASEURL_ALPHA = "go/packageOpenApiWeb/OpenApiV2";
-    const DOMAIN_ALPHA = "alphaxcottons.xinhulu.com";
-    const BASEURL_BETA = "go/packageOpenApiWeb/OpenApiV2";
-    const DOMAIN_BETA = "betaxcottons.xinhulu.com";
-    const BASEURL_IDC = "go/packageOpenApiWeb/OpenApiV2";
-    const DOMAIN_IDC = "seller.xcottons.com";
-    const ENV_ALPHA = "ALPHA";
-    const ENV_BETA = "BETA";
-    const ENV_IDC = "IDC";
-    const API_VERSION = "20250411";
-    const CLIENT_VERSION = "20250411";
+    public const  BASEURL_ALPHA = "go/packageOpenApiWeb/OpenApiV2";
+    public const  DOMAIN_ALPHA = "alphaxcottons.xinhulu.com";
+    public const  BASEURL_BETA = "go/packageOpenApiWeb/OpenApiV2";
+    public const  DOMAIN_BETA = "betaxcottons.xinhulu.com";
+    public const  BASEURL_IDC = "go/packageOpenApiWeb/OpenApiV2";
+    public const  DOMAIN_IDC = "seller.xcottons.com";
+    public const  ENV_ALPHA = "ALPHA";
+    public const  ENV_BETA = "BETA";
+    public const  ENV_IDC = "IDC";
+    public const  API_VERSION = "20250411";
+    public const  CLIENT_VERSION = "20250411";
 }
